c2119f74fc7b72215b106b0e417ac06df16eca92
Amir Talbi <amirotalbi94@gmail.com>
2024-02-19 14:09:56 +0100

    feat: adding pagination and query

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,73 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div class="h-full w-full bg-slate-800 text-center text-white ">
+    <h1 class="text-center text-4xl mb-4">Characters from Rick and Morty</h1>
+    <div class="mx-auto w-11/12">
+      <div class="grid grid-cols-5 gap-4" v-if="!loading">
+        <div v-for="(character, index) in characters" :key="`first-${character.id}`">
+          <div class="bg-slate-700 rounded-lg mx-auto flex flex-col justify-center">
+            <img :src="character.image" :alt="character.name" class="character-image" />
+            <div class="character-details">
+              <h2>{{ character.name }}</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="pagination absolute">
+        <button @click="prevPage" :disabled="currentPage === 1">Previous</button>
+        <span>Page {{ currentPage }} of {{ totalPages }}</span>
+        <button @click="nextPage" :disabled="currentPage === totalPages">Next</button>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useQuery } from '@vue/apollo-composable';
+import gql from 'graphql-tag';
+
+const currentPage = ref(1);
+const characters: any = ref([]);
+const totalPages = ref(0);
+
+const GET_CHARACTERS = gql`
+  query Characters($page: Int!) {
+    characters(page: $page) {
+      info {
+        pages
+        next
+        prev
+      }
+      results {
+        image
+        name
+      }
+    }
+  }
+`;
+
+const { onResult, loading, refetch } = useQuery(GET_CHARACTERS, {
+  page: currentPage.value,
+});
+
+onResult(({ data }) => {
+  loading.value = false;
+  characters.value = data.characters.results;
+  totalPages.value = data.characters.info.pages;
+});
+
+const nextPage = () => {
+  if (currentPage.value < totalPages.value) {
+    currentPage.value += 1;
+    refetch({ page: currentPage.value });
+  }
+};
+
+const prevPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value -= 1;
+    refetch({ page: currentPage.value });
+  }
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,114 +1,73 @@
 <template>
   <div>
-    <h1>Characters from Rick and Morty</h1>
-    <ul>
-      <li v-for="character in characters" :key="character.id">
-        <div class="character-card">
-          <img :src="character.image" :alt="character.name" class="character-image" />
-          <div class="character-details">
-            <h2>{{ character.name }}</h2>
-            <p>Status: {{ character.status }}</p>
-            <p>Species: {{ character.species }}</p>
+    <h1 class="text-center">Characters from Rick and Morty</h1>
+    <div class="mx-auto">
+      <div class="grid grid-cols-5 gap-4" v-if="!loading">
+        <div v-for="(character, index) in characters" :key="`first-${character.id}`">
+          <div class="character-card">
+            <img :src="character.image" :alt="character.name" class="character-image" />
+            <div class="character-details">
+              <h2>{{ character.name }}</h2>
+            </div>
           </div>
         </div>
-      </li>
-    </ul>
-
-    <div class="pagination">
-      <button @click="prevPage" :disabled="currentPage === 1">Previous</button>
-      <span>Page {{ currentPage }} of {{ totalPages }}</span>
-      <button @click="nextPage" :disabled="currentPage === totalPages">Next</button>
+      </div>
+      
+      <div class="pagination absolute">
+        <button @click="prevPage" :disabled="currentPage === 1">Previous</button>
+        <span>Page {{ currentPage }} of {{ totalPages }}</span>
+        <button @click="nextPage" :disabled="currentPage === totalPages">Next</button>
+      </div>
     </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, watch } from 'vue';
-import { useQuery, useApolloClient } from '@vue/apollo-composable';
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useQuery } from '@vue/apollo-composable';
 import gql from 'graphql-tag';
 
+const currentPage = ref(1);
+const characters: any = ref([]);
+const totalPages = ref(0);
+
 const GET_CHARACTERS = gql`
-  query GetAllPerso($page: String!) {
+  query Characters($page: Int!) {
     characters(page: $page) {
       info {
         pages
+        next
+        prev
       }
       results {
         image
         name
-        status
-        species
       }
     }
   }
 `;
 
-export default defineComponent({
-  setup() {
-    const currentPage = ref(1);
-    const apolloClient = useApolloClient();
-
-    const { result, refetch } = useQuery(GET_CHARACTERS, {
-      page: currentPage.value,
-      client: apolloClient,
-    });
-
-    const characters: any = ref([]);
-    const totalPages = ref(0);
-
-    watch(result, () => {
-      console.log(flatted.stringify(result.value)); // Utilisez flatted pour la sérialisation
-      if (result.value && result.value.characters) {
-        characters.value = result.value.characters.results;
-        totalPages.value = result.value.characters.info.pages;
-      }
-    });
-
-    const nextPage = () => {
-      if (currentPage.value < totalPages.value) {
-        currentPage.value += 1;
-        refetch({ page: currentPage.value, client: apolloClient });
-      }
-    };
-
-    const prevPage = () => {
-      if (currentPage.value > 1) {
-        currentPage.value -= 1;
-        refetch({ page: currentPage.value, client: apolloClient });
-      }
-    };
-
-    return { characters, nextPage, prevPage, currentPage, totalPages };
-  },
+const { onResult, loading, refetch } = useQuery(GET_CHARACTERS, {
+  page: currentPage.value,
 });
+
+onResult(({ data }) => {
+  loading.value = false;
+  characters.value = data.characters.results;
+  totalPages.value = data.characters.info.pages;
+});
+
+const nextPage = () => {
+  if (currentPage.value < totalPages.value) {
+    currentPage.value += 1;
+    refetch({ page: currentPage.value });
+  }
+};
+
+const prevPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value -= 1;
+    refetch({ page: currentPage.value });
+  }
+};
 </script>
-
-<style scoped>
-.character-card {
-  display: flex;
-  margin-bottom: 20px;
-}
-
-.character-image {
-  width: 100px;
-  height: 100px;
-  object-fit: cover;
-  margin-right: 10px;
-}
-
-.character-details {
-  display: flex;
-  flex-direction: column;
-}
-
-.pagination {
-  margin-top: 20px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-button {
-  cursor: pointer;
-}
-</style>
